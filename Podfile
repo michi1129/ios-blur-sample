622fb41b66ac1b432c557d4bd2f5a052d25b2f3e
ro <ro.michi1129@gmail.com>
2018-09-10 22:55:34 +0900

    GPUImageとCIFilterでのblurを追加
    CIFilterのウェット画像のサイズが変わってしまうのはパラメタのせいか、
    フィルタの選択の問題か

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '11.0'
+
+target 'ios-blur-sample' do
+  use_frameworks!
+
+  pod 'GPUImage'
+end
+
--- ios-blur-sample/ViewController.swift
+++ ios-blur-sample/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+import GPUImage
+
 class ViewController: UIViewController {
     @IBOutlet weak var image1: UIImageView!
     @IBOutlet weak var image2: UIImageView!
@@ -16,8 +18,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
-        
+        useGPUImage()
+        useCIFilter()
+    }
+
+    func lena() -> UIImage {
+        let img = UIImage(named: "lena.jpg")!
+        return img
+    }
+    
+    func useGPUImage() {
+        let blur = GPUImageiOSBlurFilter()
+        blur.blurRadiusInPixels = 0.5
+        image2.image = blur.image(byFilteringImage: lena())
+    }
+    
+    func useCIFilter() {
+        let old = CIImage(cgImage: lena().cgImage!)
+        guard let blur = CIFilter(name: "CIGaussianBlur") else {
+            print("cannot create filter")
+            return
+        }
+        blur.setDefaults()
+        blur.setValue(4.0, forKey: kCIInputRadiusKey)
+        blur.setValue(old, forKey: kCIInputImageKey)
+        image3.image = UIImage(ciImage: blur.outputImage!)
     }
 }
 
